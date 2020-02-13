a306e7ca36b2e0f5d3bdb072ba5486a0daa216af
Pradeepta Kumar Khatoi <PradeeptaK@citiustech.com>
2020-02-13 14:28:43 +0530

    Post module breaking issue fixed.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  providers: [PostService]
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
+  posts: any = [];
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    // Get Posts
+    this.postService.getPosts().subscribe(posts => {
+      this.posts = posts;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
