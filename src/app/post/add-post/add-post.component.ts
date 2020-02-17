e1149abdb2c2245f5e0c950c4b12e53d4080ac49
Pradeepta Kumar Khatoi <PradeeptaK@citiustech.com>
2020-02-17 16:45:56 +0530

    Implemented CanDeactivate guard for add new post page.

--- src/app/post/add-post/add-post.component.ts
+++ src/app/post/add-post/add-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-add-post',
@@ -9,6 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AddPostComponent implements OnInit {
   createPostForm: FormGroup;
   submitted = false;
+  isUpdating = false;
 
   constructor(private fb: FormBuilder) { }
 
@@ -22,8 +24,23 @@ export class AddPostComponent implements OnInit {
   // convenience getter for easy access to form fields
   get f() { return this.createPostForm.controls; }
 
+  // Implement canDeactivate method
+  canDeactivate(): Observable<boolean> | boolean {
+    console.log('isUpdating : ' + this.isUpdating);
+    console.log('dirty :' + this.createPostForm.dirty);
+    if (this.isUpdating || this.createPostForm.dirty) {
+      if (confirm('Discard changes for Person?')) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+
   onSubmit() {
     this.submitted = true;
+    this.isUpdating = true;
     if (this.createPostForm.valid) {
       console.log(this.createPostForm.value);
     } else {
--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PostComponent } from './post.component';
 import { AddPostComponent } from './add-post/add-post.component';
+import { CanDeactivateGuard } from '../_helpers/can-deactivate.guard';
 
 const routes: Routes = [
   { path: '', component: PostComponent },
-  { path: 'addpost', component: AddPostComponent }
+  { path: 'addpost', component: AddPostComponent, canDeactivate: [CanDeactivateGuard] }
 ];
 
 @NgModule({
