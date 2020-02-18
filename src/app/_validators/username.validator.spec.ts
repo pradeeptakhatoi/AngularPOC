47f42b917d8a7bbf0afc5aebe579bae44706f1b9
Pradeepta Kumar Khatoi <PradeeptaK@citiustech.com>
2020-02-18 14:18:19 +0530

    Unit test case for username custom validator.

---
+++ src/app/_validators/username.validator.spec.ts
@@ -0,0 +1,32 @@
+import { ValidateUsername } from './username.validator';
+import { FormControl } from '@angular/forms';
+
+describe('Validate Username', () => {
+  const control = new FormControl('input');
+
+  it('empty username should be treated as valid', () => {
+    control.setValue('');
+    expect(ValidateUsername(control)).toBeNull();
+  });
+
+  it('username less than 8 char is invalid', () => {
+    control.setValue('1234567');
+    expect(ValidateUsername(control)).toEqual({ validUsername: true });
+  });
+
+  it('username more than 20 char is invalid', () => {
+    control.setValue('1234567890123456789012');
+    expect(ValidateUsername(control)).toEqual({ validUsername: true });
+  });
+
+  it('username containg space or any special char are invalid', () => {
+    control.setValue('qwer @123');
+    expect(ValidateUsername(control)).toEqual({ validUsername: true });
+  });
+
+  it('username containing alpaha-numeric & bet 8-20 are valid', () => {
+    control.setValue('pradeeptakumarkhatoi');
+    expect(ValidateUsername(control)).toBeNull();
+  });
+
+});
