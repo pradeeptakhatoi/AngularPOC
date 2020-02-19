159226b9019db21f3ef95d9a4ef48386eb292bc7
Pradeepta Kumar Khatoi <PradeeptaK@citiustech.com>
2020-02-19 19:09:59 +0530

    Implemented OnPush change detection  using  ngDoCheck lifecyle hook.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, DoCheck, ChangeDetectorRef } from '@angular/core';
 import { User } from '../_models';
 import { AuthenticationService } from '../_services';
 import { Router } from '@angular/router';
@@ -6,19 +6,39 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, DoCheck {
   title = 'AngularPOC';
   loading = false;
   @Input() user: User;
+  userUpdated = false;
 
   constructor(
     private router: Router,
-    private authenticationService: AuthenticationService) { }
+    private authenticationService: AuthenticationService,
+    private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
-    // Code
+    console.log('Before Update : ' + JSON.stringify(this.user));
+    setTimeout(() => {
+      if (this.user) {
+        this.user.firstName = 'Pradeepta';
+        this.user.lastName = 'Khatoi';
+        this.user.username = 'pradeepta';
+        this.userUpdated = true;
+      }
+      console.log('After Update : ' + JSON.stringify(this.user));
+    }, 2000);
+  }
+
+  ngDoCheck() {
+    // check for object mutation
+    if (this.userUpdated) {
+      this.userUpdated = !this.userUpdated;
+      this.cd.markForCheck();
+    }
   }
 
   logout() {
