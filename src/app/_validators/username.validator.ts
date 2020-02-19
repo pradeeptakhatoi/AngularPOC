0ee0cf72269b8a0729982120460d3e50a1e0251b
Pradeepta Kumar Khatoi <PradeeptaK@citiustech.com>
2020-02-19 16:12:25 +0530

    Username validator  min length set to 4 instead of 8

--- src/app/_validators/username.validator.ts
+++ src/app/_validators/username.validator.ts
@@ -1,7 +1,7 @@
 import { AbstractControl } from '@angular/forms';
 
 export function ValidateUsername(control: AbstractControl) {
-  const regx = new RegExp('^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$');
+  const regx = new RegExp('^(?=.{4,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$');
   if (control.value && !regx.test(control.value)) {
     return { validUsername: true };
   }
