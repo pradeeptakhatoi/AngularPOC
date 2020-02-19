832971807d5b795d40f2fd878ab7374a5465cb56
Pradeepta Kumar Khatoi <PradeeptaK@citiustech.com>
2020-02-19 20:51:06 +0530

    Implemented HostListener & HostBinding for custom Highlight  directive.

--- src/app/post/highlight-long-string.directive.ts
+++ src/app/post/highlight-long-string.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, AfterViewInit } from '@angular/core';
+import { Directive, ElementRef, Input, AfterViewInit, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightLongString]'
@@ -7,6 +7,7 @@ export class HighlightLongStringDirective implements AfterViewInit {
 
   @Input() str: string;
   @Input() len: number;
+  @HostBinding('style.background') background: string;
 
   constructor(private eRef: ElementRef) { }
 
@@ -14,6 +15,15 @@ export class HighlightLongStringDirective implements AfterViewInit {
     if (this.str && this.str.length > this.len) {
       this.eRef.nativeElement.style.border = '1px dotted red';
     }
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.background = 'yellow'; // Using @HostBinding
+    // this.eRef.nativeElement.style.background = 'yellow'; // Using ElementRef => nativeElement
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.background = 'white';
   }
 
 }
