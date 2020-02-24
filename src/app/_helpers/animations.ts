11262ef270643a5dfdc70ee76965595526370761
Pradeepta Kumar Khatoi <PradeeptaK@citiustech.com>
2020-02-24 19:03:43 +0530

    Animation using keyframe.

--- src/app/_helpers/animations.ts
+++ src/app/_helpers/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 export let fade = trigger('fade', [
   state('void', style({ opacity: 0 })),
@@ -6,6 +6,17 @@ export let fade = trigger('fade', [
     animate('0.5s ease-out', style({ transform: 'translateX(100px)' }))
   ]),
   transition(':leave', [
-    animate('0.5s ease-in', style({ transform: 'translateX(-100px)' }))
+    animate('0.5s ease-in', keyframes([
+      style({
+        offset: 0.2,
+        opacity: 1,
+        transform: 'translateX(20px)',
+      }),
+      style({
+        offset: 1,
+        opacity: 0,
+        transform: 'translateX(-100%)',
+      }),
+    ]))
   ])
 ]);
