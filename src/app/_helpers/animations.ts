ddcc009aab1add826b9eb96c83d01ca29df6c3e3
Pradeepta Kumar Khatoi <PradeeptaK@citiustech.com>
2020-02-24 18:49:16 +0530

    Implemented easing in animation.

--- src/app/_helpers/animations.ts
+++ src/app/_helpers/animations.ts
@@ -2,7 +2,10 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 
 export let fade = trigger('fade', [
   state('void', style({ opacity: 0 })),
-  transition(':enter, :leave', [
-    animate(1000)
+  transition(':enter', [
+    animate('0.5s ease-out', style({ transform: 'translateX(100px)' }))
+  ]),
+  transition(':leave', [
+    animate('0.5s ease-in', style({ transform: 'translateX(-100px)' }))
   ])
 ]);
